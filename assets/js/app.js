fd1b528fce1f50458ee7d05016e3bfc96c423a1d
Wilson Lam <48064335+clam1987@users.noreply.github.com>
2024-11-15 14:10:09 -0800

    top of website done, trying to figure out how to animate with gsap

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const elems = document.querySelectorAll('.parallax');
-    M.Parallax.init(elems);
+document.addEventListener("DOMContentLoaded", function () {
+  const elems = document.querySelectorAll(".parallax");
+  M.Parallax.init(elems);
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to(".hero-banner", {
+    scale: 0, // Shrinks the banner
+    y: "100vh", // Moves the banner to the bottom of the viewport
+    scrollTrigger: {
+      trigger: ".container", // Start effect when .content comes into view
+      start: "top top", // At the top of the .content
+      end: "bottom top", // End effect at the bottom of the .content
+      scrub: true, // Smooth animation linked to scroll
+    },
   });
+});
